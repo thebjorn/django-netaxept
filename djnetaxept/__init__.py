06b29d9a7cf40612bd8261f957474ba0b2d34cf8
Bjorn Pettersen <bp@datakortet.no>
2013-11-28 17:17:05 +0100

    Minimal admin interface. Up version number.

--- djnetaxept/__init__.py
+++ djnetaxept/__init__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-VERSION = (0, 8, 0, 'beta', 1)
+VERSION = (0, 8, 1)
 
-def get_version(): # pragma: no cover
+
+def get_version():  # pragma: no cover
     version = '%s.%s' % (VERSION[0], VERSION[1])
     if VERSION[2]:
         version = '%s.%s' % (version, VERSION[2])
--- djnetaxept/admin.py
+++ djnetaxept/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from djnetaxept.models import NetaxeptPayment, NetaxeptTransaction
 
-admin.site.register(NetaxeptPayment)
-admin.site.register(NetaxeptTransaction)
+
+class NetaxeptPaymentOptions(admin.ModelAdmin):
+    list_display = """id transaction_id amount currencycode description
+                      ordernumber flagged responsecode responsesource
+                      responsetext""".split()
+
+
+class NetaxeptTransactionOptions(admin.ModelAdmin):
+    list_display = """id transaction_id operation amount flagged responsecode
+                      responsesource responsetext""".split()
+
+
+admin.site.register(NetaxeptPayment, NetaxeptPaymentOptions)
+admin.site.register(NetaxeptTransaction, NetaxeptTransactionOptions)
